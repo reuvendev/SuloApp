b8a812210938a9adaaf2a4772246bb64fc348778
reuvendev <103184304+reuvendev@users.noreply.github.com>
2026-04-26 17:14:45 +0800

    Added signup activity

--- app/src/main/java/com/mazedev/sulo/LoginPage.java
+++ app/src/main/java/com/mazedev/sulo/LoginPage.java
@@ -1,6 +1,10 @@
 package com.mazedev.sulo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginPage extends AppCompatActivity {
@@ -9,5 +13,17 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        TextView textViewSignup = findViewById(R.id.textView_signUp);
+
+        textViewSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, SignUpPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/mazedev/sulo/SignUpPage.java
@@ -0,0 +1,30 @@
+package com.mazedev.sulo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up_page);
+
+        TextView textViewlogin = findViewById(R.id.textView_signUp);
+
+        textViewlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpPage.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
